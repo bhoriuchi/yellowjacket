e685e12486e08927373ac61d9ed187d0e5a141a9
Branden Horiuchi <bhoriuchi@gmail.com>
2016-09-04 12:04:12 -0700

    * changing command line parser

--- src/app/options.js
+++ src/app/options.js
@@ -1,42 +1,47 @@
-import GetOpt from 'node-getopt'
+import NestedOpts from 'nested-opts'
+
+let config = {
+  options: {},
+  commands: {
+    runner: {
+      commands: {
+        add: {
+          options: {
+            host: { type: 'String' },
+            port: { type: 'Int' }
+          }
+        },
+        remove: {
+          options: {
+            id: { type: 'String' },
+            host: { type: 'String' },
+            port : { type: 'Int' }
+          }
+        },
+        update: {
+          options: {
+            id: { type: 'String' },
+            host: { type: 'String' },
+            port: { type: 'Int' },
+            zone: { type: 'String' },
+            state: { type: 'String' }
+          }
+        },
+        start: {
+          options: {
+            id: { type: 'String' },
+            host: { type: 'String' },
+            port: { type: 'Int' },
+            loglevel: { type: 'String' },
+            logfile: { type: 'String' }
+          }
+        }
+      }
+    }
+  }
+}
+
 
 export default function () {
-  let getopt = new GetOpt([
-    ['', 'config=ARG', 'Configuration file'],
-    ['', 'add', 'Adds a new node'],
-    ['', 'remove', 'Removes an existing node'],
-    ['', 'update', 'Update node configuration'],
-    ['', 'start', 'Start the server'],
-    ['', 'cmd=ARG', 'Sends a command'],
-    ['', 'id=ARG', 'ID of node'],
-    ['h', 'host=ARG', 'Host name or IP'],
-    ['p', 'port=ARG', 'Port number (default 8080)'],
-    ['', 'loglevel=ARG', 'Log level (defaults to "info")'],
-    ['', 'logfile=ARG', 'Log file location']
-  ])
-
-  getopt.setHelp(`
-Usage: s2fcli [operation] [command | options...]
-  operation:
-    --add          : adds a new node
-    --remove       : removes an existing node
-    --update       : updates an existing node
-    --start        : starts a node
-    --cmd          : sends a command
-  options:
-    -h, --host     : host name or ip
-    -p, --port     : host port (default 8080)
-        --id       : ID of a node
-        --loglevel : Logging level (silent, trace, debug, info, warn, error, fatal)
-        --logfile  : Log file location
-  commands:
-    stop           : stops node
-    restart        : restarts node
-    info           : prints info about node
-    list           : prints cluster node config
-    changerole     : changes node role
-    maintenance    : pust node into maintenance
-    status         : get node status directly from node
-`)
-  return getopt
+  return NestedOpts(config)
 }
